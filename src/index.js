caabb816d96f22ff29cb93333eb0c5da65ceb04a
shahalam13 <shahalam7303@gmail.com>
2020-08-18 11:41:42 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore ,combineReducers } from 'redux';
-import '../node_modules/fullcalendar/dist/fullcalendar.min.js';
-import  UIreducer from './store/reducers/reducer';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, combineReducers } from "redux";
+import "../node_modules/fullcalendar/dist/fullcalendar.min.js";
+import UIreducer from "./store/reducers/reducer";
+import { Provider } from "react-redux";
 
 const rootReducer = combineReducers({
-    ui_red:UIreducer,
+  ui_red: UIreducer,
 });
 const store = createStore(rootReducer);
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter basename={"/SMS-react-admin/"}>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
 serviceWorker.unregister();
