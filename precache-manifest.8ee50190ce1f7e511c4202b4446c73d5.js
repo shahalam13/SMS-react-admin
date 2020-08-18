c8bcb6a864bd93dd1bad12d085c6ea9a07fd9dbc
shahalam13 <shahalam7303@gmail.com>
2020-08-18 10:47:24 +0600

    Updates

---
+++ precache-manifest.8ee50190ce1f7e511c4202b4446c73d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3aba7f1940be5223b2",
+    "url": "/SMS-react-admin/static/js/main.1e3aba7f.chunk.js"
+  },
+  {
+    "revision": "d1193fe6da60fa30a4d7",
+    "url": "/SMS-react-admin/static/css/1.d164bbd6.chunk.css"
+  },
+  {
+    "revision": "d1193fe6da60fa30a4d7",
+    "url": "/SMS-react-admin/static/js/1.d1193fe6.chunk.js"
+  },
+  {
+    "revision": "8630ec2746de6a1b039a",
+    "url": "/SMS-react-admin/static/js/runtime~main.8630ec27.js"
+  },
+  {
+    "revision": "1fd9c1bacad964d39d53cb3edb81d056",
+    "url": "/SMS-react-admin/index.html"
+  }
+];
